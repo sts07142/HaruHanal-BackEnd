8fa459efc67aa040f6781b4f37fd7963476165bf
codrin2 <hssang127@gmail.com>
2023-11-03 16:48:56 +0900

    feat : Base, Review, User Entity 추가

--- src/main/java/com/example/haruhanal/entity/Review.java
+++ src/main/java/com/example/haruhanal/entity/Review.java
@@ -1,4 +1,35 @@
 package com.example.haruhanal.entity;
 
-public class Review {
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String text;
+    private String review_image_url;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Review(Long id, String title, String text, String review_image_url) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.review_image_url = review_image_url;
+    }
 }
--- src/main/java/com/example/haruhanal/entity/User.java
+++ src/main/java/com/example/haruhanal/entity/User.java
@@ -1,4 +1,60 @@
 package com.example.haruhanal.entity;
 
-public class User {
+import com.example.haruhanal.enums.Gender;
+import com.example.haruhanal.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(nullable = false)
+    private String UID;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Column(nullable = false)
+    private String phone;
+    private Integer age;
+    private String address;
+    private String condition;
+    private Integer subscribe;
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.REMOVE)
+    private List<Review> reviews = new ArrayList<>();
+    @OneToMany(mappedBy = "user",cascade = CascadeType.REMOVE)
+    private List<Question> questions = new ArrayList<>();
+
+    @Builder
+    public User(Long id, String UID, String password, String name, Gender gender, String phone, Integer age, String address, String condition, Integer subscribe, UserRole userRole) {
+        this.id = id;
+        this.UID = UID;
+        this.password = password;
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+        this.age = age;
+        this.address = address;
+        this.condition = condition;
+        this.subscribe = subscribe;
+        this.userRole = userRole;
+    }
+
 }
