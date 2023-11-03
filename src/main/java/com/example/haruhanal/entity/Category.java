6cbe58168fabd6c7241b976a56fe5ece95338886
heesang <hssang127@gmail.com>
2023-11-03 23:41:53 +0900

    feat: entity 완료

--- src/main/java/com/example/haruhanal/entity/Category.java
+++ src/main/java/com/example/haruhanal/entity/Category.java
@@ -1,4 +1,23 @@
 package com.example.haruhanal.entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+    private String name;
+
+    @Builder
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/main/java/com/example/haruhanal/entity/Category_item.java
+++ src/main/java/com/example/haruhanal/entity/Category_item.java
@@ -1,4 +1,17 @@
 package com.example.haruhanal.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Category_item {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
